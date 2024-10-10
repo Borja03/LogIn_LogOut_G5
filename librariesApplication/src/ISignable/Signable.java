e57167081c7650254d4cfe554258168d7c324dc7
Alder <aldersanchez163@gmail.com>
2024-10-10 08:06:58 +0200

    javadoc

--- librariesApplication/src/ISignable/Signable.java
+++ librariesApplication/src/ISignable/Signable.java
@@ -14,16 +14,16 @@ public interface Signable {
     /**
      * Método para registrar un nuevo usuario.
      * 
-     * @param user El objeto User que contiene la información del nuevo usuario.
+     * @return User es el objeto que se retorna en el metodo
      * @throws Exception Si ocurre un error durante el registro.
      */
-    public void signUp(User user) throws Exception;
+    public User signUp() throws Exception;
 
     /**
      * Método para iniciar sesión de un usuario existente.
      * 
-     * @param user El objeto User que contiene la información de inicio de sesión.
+     * @return User es el objeto que se retorna en el metodo
      * @throws Exception Si ocurre un error durante el inicio de sesión.
      */
-    public void signIn(User user) throws Exception;
+    public User signIn() throws Exception;
 }
