8ed013006c5ac5583337ec749286eab9d5d37440
AdrianGarciaCaballero <144772061+AdrianGarciaCaballero@users.noreply.github.com>
2024-10-11 08:39:32 +0200

    InvalidDniFormatException

---
+++ clientApplication/src/exception/InvalidDniFormatException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ *
+ * @author 2dam
+ */
+public class InvalidDniFormatException extends Exception {
+    public InvalidDniFormatException(String message) {
+        super(message);
+    }
+}
