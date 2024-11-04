7d1ea6136d77791e9fe021252b6445b6414c53f8
Alder <aldersanchez163@gmail.com>
2024-11-04 08:13:06 +0100

    libraries javadoc

--- librariesApplication/src/exception/IncorrectCredentialsException.java
+++ librariesApplication/src/exception/IncorrectCredentialsException.java
@@ -1,15 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exception;
 
 /**
- *
+ * Excepción que indica que las credenciales proporcionadas son incorrectas.
+ * 
+ * <p>Esta clase extiende {@link Exception} y se utiliza para señalar
+ * situaciones en las que un usuario intenta acceder al sistema con
+ * credenciales que no son válidas.</p>
+ * 
  * @author Alder
  */
 public class IncorrectCredentialsException extends Exception {
+    
+    /**
+     * Crea una nueva instancia de IncorrectCredentialsException con un mensaje específico.
+     *
+     * @param message el mensaje que describe el motivo de la excepción
+     */
     public IncorrectCredentialsException(String message) {
         super(message);
     }
