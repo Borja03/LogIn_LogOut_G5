9868f1b2b7a07e27bc170ac116943858d4334c05
Alder <aldersanchez163@gmail.com>
2024-11-04 08:20:18 +0100

    exception client

--- clientApplication/src/exception/InvalidPasswordFormatException.java
+++ clientApplication/src/exception/InvalidPasswordFormatException.java
@@ -1,15 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exception;
 
 /**
- *
+ * Excepción que indica que el formato de una contraseña es inválido.
+ * 
+ * <p>Esta clase extiende {@link Exception} y se utiliza para señalar
+ * situaciones en las que una contraseña proporcionada no cumple con los
+ * criterios de formato válidos establecidos por la aplicación, como longitud
+ * mínima o inclusión de caracteres especiales.</p>
+ * 
  * @author Omar
  */
 public class InvalidPasswordFormatException extends Exception {
+    
+    /**
+     * Crea una nueva instancia de InvalidPasswordFormatException con un mensaje específico.
+     *
+     * @param message el mensaje que describe el motivo de la excepción
+     */
     public InvalidPasswordFormatException(String message) {
         super(message);
     }
